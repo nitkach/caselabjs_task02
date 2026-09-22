cd81fc8b9bc346a10ea86c2469e8b61910c5d8ba
nitkach <nikita.tkachev2001@gmail.com>
2026-09-22 06:08:22 +0300

    feat: postman collections and better logging

--- src/middleware/requestLogger.ts
+++ src/middleware/requestLogger.ts
@@ -1,7 +1,14 @@
 import type { NextFunction, Request, Response } from "express";
 
-export const requestLogger = (req: Request, _res: Response, next: NextFunction): void => {
-  const timestamp = new Date().toISOString();
-  console.log(`[${timestamp}] ${req.method} ${req.originalUrl}`);
-  next();
+export const requestLogger = (req: Request, res: Response, next: NextFunction): void => {
+    const startTime = process.hrtime.bigint();
+
+    res.on("finish", () => {
+        const timestamp = new Date().toISOString();
+        const durationMs = Number(process.hrtime.bigint() - startTime) / 1_000_000;
+
+        console.log(`[${timestamp}] ${req.method} ${req.originalUrl} ${req.requestId} - code: ${res.statusCode} ${Math.round(durationMs * 100) / 100}ms`)
+    });
+
+    next();
 };
